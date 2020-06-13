0758cee7feca113772783ab938ccc0cad72aee37
David Siebo <zentraal@gmail.com>
2020-06-13 07:36:23 -0700

    missing import for Nav

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import Nav from './Nav';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import SocialButton from './SocialButton'
