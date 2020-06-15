34a93f6f5995f23b885df438222b7208f913c3c2
David Siebo <zentraal@gmail.com>
2020-06-15 13:57:11 -0700

    improve router integration with drawer nav

--- src/App.js
+++ src/App.js
@@ -2,37 +2,120 @@ import React from 'react';
 import './App.css';
 import Welcome from './Welcome';
 import Papers from './Papers';
+import Graphs from './Graphs';
+import clsx from 'clsx';
 import GoogleBtn from './GoogleBtn';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AccessibilityIcon from '@material-ui/icons/Accessibility';
+import AccountBalanceIcon from '@material-ui/icons/AccountBalance';
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import HomeIcon from '@material-ui/icons/Home';
+import IconButton from '@material-ui/core/IconButton';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
+import PollIcon from '@material-ui/icons/Poll';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Link
+  Link,
+  useHistory
 } from "react-router-dom";
 
+const drawerWidth = 240;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    flexGrow: 1,
+    display: 'flex',
+  },
+  appBar: {
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+  },
+  appBarShift: {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: drawerWidth,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
   },
   menuButton: {
     marginRight: theme.spacing(2),
   },
-  title: {
+  hide: {
+    display: 'none',
+  },
+  drawer: {
+    width: drawerWidth,
+    flexShrink: 0,
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  drawerHeader: {
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'flex-end',
+  },
+  content: {
     flexGrow: 1,
+    padding: theme.spacing(3),
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    marginLeft: -drawerWidth,
+  },
+  contentShift: {
+    transition: theme.transitions.create('margin', {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    marginLeft: 0,
   },
 }));
 
 
 function App() {
   const classes = useStyles();
+  const theme = useTheme();
+  const [open, setOpen] = React.useState(false);
+  const history = useHistory();
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+  function linkHome() {
+    history.push("/sharestats");
+  };
+  function linkPIs() {
+    history.push("/sharestats/papers");
+  };
+  function linkOrgs() {
+    history.push("/sharestats/papers");
+  };
+  function linkGraphs() {
+    history.push("/sharestats/graphs");
+  };
   return (
     <div className="App">
 
@@ -40,32 +123,88 @@ function App() {
         <div>
 
           <div className={classes.root}>
-            <AppBar position="static">
+            <CssBaseline />
+              <AppBar
+                position="fixed"
+                className={clsx(classes.appBar, {
+                  [classes.appBarShift]: open,
+                })}
+              >
               <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                <IconButton
+                  color="inherit"
+                  aria-label="open drawer"
+                  onClick={handleDrawerOpen}
+                  edge="start"
+                  className={clsx(classes.menuButton, open && classes.hide)}
+                >
                   <MenuIcon />
                 </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                  <Link to="/sharestats">Home</Link>
-                </Typography>
-                <Typography variant="h6" className={classes.title}>
-                  <Link to="/sharestats/papers">Papers</Link>
+                <Typography variant="h6" noWrap>
+                  NIMH OHBM
                 </Typography>
                 <GoogleBtn/>
               </Toolbar>
             </AppBar>
+            <Drawer
+              className={classes.drawer}
+              variant="persistent"
+              anchor="left"
+              open={open}
+              classes={{
+                paper: classes.drawerPaper,
+              }}
+              >
+              <div className={classes.drawerHeader}>
+                <IconButton onClick={handleDrawerClose}>
+                  {theme.direction === 'ltr' ? <ChevronLeftIcon /> : <ChevronRightIcon />}
+                </IconButton>
+              </div>
+              <Divider />
+              <List>
+                <ListItem button key="Home" onClick={linkHome}>
+                  <ListItemIcon><HomeIcon /></ListItemIcon>
+                  <ListItemText primary="Home" />
+                </ListItem>
+                <ListItem button key="Graphs" onClick={linkGraphs}>
+                  <ListItemIcon><AccessibilityIcon /></ListItemIcon>
+                  <ListItemText primary="Graphs" />
+                </ListItem>
+                <ListItem button key="Institutions" onClick={linkOrgs}>
+                  <ListItemIcon><AccountBalanceIcon /></ListItemIcon>
+                  <ListItemText primary="Institutions" />
+                </ListItem>
+                <ListItem button key="PIs" onClick={linkPIs}>
+                  <ListItemIcon><AccessibilityIcon /></ListItemIcon>
+                  <ListItemText primary="PIs" />
+                </ListItem>
+              </List>
+              </Drawer>
+
+              <main
+                className={clsx(classes.content, {
+                  [classes.contentShift]: open,
+                })}
+              >
+                <div className={classes.drawerHeader} />
+                <Switch>
+                  <Route path="/sharestats/papers">
+                    <Papers />
+                  </Route>
+                  <Route path="/sharestats/graphs">
+                    <Graphs />
+                  </Route>
+                  <Route path="/sharestats">
+                    <Welcome />
+                  </Route>
+                </Switch>
+              </main>
+
           </div>
 
           {/* A <Switch> looks through its children <Route>s and
               renders the first one that matches the current URL. */}
-          <Switch>
-            <Route path="/sharestats/papers">
-              <Papers />
-            </Route>
-            <Route path="/sharestats">
-              <Welcome />
-            </Route>
-          </Switch>
+
 
         </div>
       </Router>
---
+++ src/Graphs.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Graphs() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Graphs here
+      </header>
+    </div>
+  );
+}
+
+export default Graphs;
